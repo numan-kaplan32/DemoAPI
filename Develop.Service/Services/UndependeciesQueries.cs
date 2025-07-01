d04c10a3e4625fa1e9ed48aa34f7b0018078e7ef
Numan Kaplan <numankaplan32@gmail.com>
2025-07-01 18:16:47 +0300

    Add files via upload

---
+++ Develop.Service/Services/UndependeciesQueries.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Develop.Data.DbContextFolder;
+using Develop.Data.Tables;
+using Microsoft.EntityFrameworkCore;
+
+namespace Develop.Service.Services
+{
+    public class UndependenciesQueries
+    {
+        private readonly APIDbContext _dbServices;
+
+        public UndependenciesQueries(APIDbContext aPIDb)
+        {
+            _dbServices = aPIDb;
+        }
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            return await _dbServices.Users.FirstOrDefaultAsync(x => x.Email == email);
+        }
+    }
+}
