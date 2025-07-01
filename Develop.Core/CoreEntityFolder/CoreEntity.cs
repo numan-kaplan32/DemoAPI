3e57f2ca1da6c0e914d007d8d97c6df087939dc0
Numan Kaplan <numankaplan32@gmail.com>
2025-07-01 18:21:17 +0300

    Add files via upload

---
+++ Develop.Core/CoreEntityFolder/CoreEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Develop.Core.CoreEntityFolder
+{
+    public class CoreEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; }
+     
+    }
+}
---
+++ Develop.Core/InterfaceCore/ICoreService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Develop.Core.CoreEntityFolder;
+
+namespace Develop.Core.InterfaceCore
+{
+   public interface ICoreService<T> where T : CoreEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<bool> CreateAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+        IQueryable<T> FilterData();
+        Task<bool> SaveAsync();
+    }
+}
